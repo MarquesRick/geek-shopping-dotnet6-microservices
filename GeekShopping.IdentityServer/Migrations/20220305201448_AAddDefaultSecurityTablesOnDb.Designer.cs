4abc9b97b0f7156674a074819dd86f22d9758992
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-03-05 17:17:03 -0300

    create  migration for create database and tables on geek_identity

--- GeekShopping.IdentityServer/Migrations/20220305201448_AAddDefaultSecurityTablesOnDb.Designer.cs
+++ GeekShopping.IdentityServer/Migrations/20220305201448_AAddDefaultSecurityTablesOnDb.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GeekShopping.IdentityServer.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GeekShopping.IdentityServer.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20220305201448_AAddDefaultSecurityTablesOnDb")]
+    partial class AAddDefaultSecurityTablesOnDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
